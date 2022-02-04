f2551b53cffb242ca25d48afdc2a1ebc1cee07e0
shalu sweetiee <shalu sweetiee@shalini>
2022-02-04 16:50:44 +0530

    com.java1 package added into a project

--- 04.02.2022/src/com/java1/Example2.java
+++ 04.02.2022/src/com/java1/Example2.java
@@ -1,9 +1,9 @@
-package com.java;
+package com.java1;
 
 public class Example2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hi,My Dear Friends");
+		System.out.println("I am Nandhini Here");
 
 	}
 
